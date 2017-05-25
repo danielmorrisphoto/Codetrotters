916a71a7075b70e72108f5a7451063260ba1c534
Daniel Morris <daniel@danielmorrisphoto.com>
2017-05-25 18:11:10 -0400

    started Final Project Have not finished Mod3 ex1 and 2…..

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,8 +1,18 @@
-function hello() {
-    var name=prompt ('What is your name?');
-    alert(name);
+var result = promt ("How feet do you have?");
+    if (result === true) {
+        alert("WOOHOO!!!!");
+    } else {
+        alert("You suck!");
+    }
 
-}
 
-document.getElementById('button').addEventListener('click',hello);
+
+
+// function hello() {
+//     var name=prompt ('What is your name?');
+//     alert(name);
+
+// }
+
+// document.getElementById('button').addEventListener('click',hello);
 
