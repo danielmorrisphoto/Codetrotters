0f65045b6eb8f5cfa5e4784e130234b37f20db85
Daniel Morris <daniel@danielmorrisphoto.com>
2017-05-25 18:11:33 -0400

    no message

---
+++ JavaScript/main2.js
@@ -0,0 +1,32 @@
+var validation = function() {
+    var firstName = document.getElementById("first-name").value;
+    var firstName = document.getElementById("last-name").value;
+    var firstName = document.getElementById("email").value;
+    var firstName = document.getElementById("password").value;
+    var firstName = document.getElementById("confirm-password").value;
+    var isRestrationsSuccesful = true;
+    
+    if (firstName === ""){
+        alert("invalid name!");
+    }
+    if (lastName === ""){
+        alert("invalid name!");
+    }
+    if (firstName === ""){
+        alert("invalid name!");
+    }
+    if (email === ""){
+        alert("invalid name!");
+    }
+    if (firstName === ""){
+        alert("invalid name!");
+    }
+}
+
+
+
+
+
+// We want each form to validate if it is valid
+// also to make sure that the password and password confirm is the same.
+// the buttom of register runs all the funcitons    
